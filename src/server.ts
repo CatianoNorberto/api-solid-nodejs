448de1f670f4b12d2d3843da7ee437f9ad2322a0
CatianoNorberto <catianonorberto@gmail.com>
2023-08-30 16:41:09 -0300

    feat: carregando variáveis ambientes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import { app } from './app'
-// import { env } from './env'
+import { env } from './env'
 
 app
   .listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('🚀 HTTP server Running on port 3333')
